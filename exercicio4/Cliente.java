fecdd9f11eeb7cf7706683ac2430842a093bb726
Roberto <161033388+Hubierto@users.noreply.github.com>
2024-08-22 11:35:50 -0300

    Add files via upload

---
+++ exercicio4/Cliente.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exercicio4;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Cliente {
+    
+    private int protocoloAtendimento;
+
+    public Cliente(int protocoloAtendimento) {
+        this.protocoloAtendimento = protocoloAtendimento;
+    }
+
+    public int getProtocoloAtendimento() {
+        return protocoloAtendimento;
+    }
+
+    public void setProtocoloAtendimento(int protocoloAtendimento) {
+        this.protocoloAtendimento = protocoloAtendimento;
+    }
+    
+    
+    
+}
